1d61713a1e05a0333bc30baec57c9a2461483830
Wilder Trujillo <nadie437@gmail.com>
2022-05-19 20:46:20 -0500

    first commid basic web

--- src/main/java/com/example/herokudemo/HerokudemoApplication.java
+++ src/main/java/com/example/herokudemo/HerokudemoApplication.java
@@ -2,9 +2,19 @@ package com.example.herokudemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class HerokudemoApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HerokudemoApplication.class, args);
